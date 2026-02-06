1316ddd795b8b8757723c0876c1c3c2d80ba56e2
Stephan Gilles <153995852+SteGi78@users.noreply.github.com>
2026-02-06 18:58:21 +0100

    Provide HttpClient testing in AppComponent spec

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideHttpClientTesting(), provideRouter([])],
     }).compileComponents();
   });
 
